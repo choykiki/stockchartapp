62f396951b5ec54b95d7597685f712a6238d0974
patipol <patipol@khroton.com>
2017-02-11 16:55:48 +0700

    2 text again

--- chatbot.php
+++ chatbot.php
@@ -22,6 +22,11 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 
+			$message = [
+            {"type":"text", "text":$text},
+            {"type":"text", "text":$text}
+        	];
+
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
