faf8c8c9cb028e8c6eab227ecc1db660a2472c31
patipol <patipol@khroton.com>
2017-02-12 12:44:55 +0700

    test chatbot

--- chatbot.php
+++ chatbot.php
@@ -27,15 +27,14 @@ if (!is_null($events['events'])) {
 
             $myvars = 'text=' . $text . '&replyToken=' . $replyToken;
 
-                    $ch = curl_init( $urlApi );
-                    curl_setopt( $ch, CURLOPT_POST, 1);
-                    curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-                    curl_setopt( $ch, CURLOPT_POSTFIELDS, $myvars);
-                    curl_setopt( $ch, CURLOPT_HEADER, 0);
-                    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-                    $reponse = curl_exec($ch);
-                    $reponse = json_encode($reponse);
-                    echo $reponse;
+                    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $myvars);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$reponse = curl_exec($ch);
+			curl_close($ch);
+                  
 
 
 			// $messages = [
