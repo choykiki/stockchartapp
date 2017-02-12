793045d6ae1622b137bcdb828db6e07e1a28566c
patipol <patipol@khroton.com>
2017-02-12 09:44:08 +0700

    ss

--- chatbot.php
+++ chatbot.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
 			// ];
 			$messages = [
 				'type' => 'image',
-				'originalContentUrl' => 'http://img.youtube.com/vi/uBg8YGWzDiw/1.jpg',
+				'originalContentUrl' => 'https://graph.yuantathai.com/InvestorWebCharts/Modules/Standard.aspx?Provider=DDC&symbolnsources=ROJNA*BKK&Size=980*882&End=20170212&Start=&period=Daily&interval=1&Cycle=DAY1&Type=3&&username=KKS&password=KKS1234&pwdencode=false&Scale=0&EMA=10;50&MA=&OVER=;;OverlayV!;;&IND=MACD(12,26,9);RSI(14);;;&&COMP=&ShowBar=100&max=800&Skin=KKSLight&Layout=2Line;Default;Price;HisDate&Width=1&TimeStamp=17;17;1486808228.251',
 				'previewImageUrl' => 'https://graph.yuantathai.com/InvestorWebCharts/Modules/Standard.aspx?Provider=DDC&symbolnsources=ROJNA*BKK&Size=980*882&End=20170212&Start=&period=Daily&interval=1&Cycle=DAY1&Type=3&&username=KKS&password=KKS1234&pwdencode=false&Scale=0&EMA=10;50&MA=&OVER=;;OverlayV!;;&IND=MACD(12,26,9);RSI(14);;;&&COMP=&ShowBar=100&max=800&Skin=KKSLight&Layout=2Line;Default;Price;HisDate&Width=1&TimeStamp=17;17;1486808228.251',
 			];
 
