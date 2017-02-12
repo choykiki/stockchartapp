e2ff08e3349495ae5fe079935404b50cace4cf75
patipol <patipol@khroton.com>
2017-02-12 09:50:41 +0700

    reply token

--- chatbot.php
+++ chatbot.php
@@ -21,6 +21,8 @@ if (!is_null($events['events'])) {
 			// 	'type' => 'text',
 			// 	'text' => $text
 			// ];
+
+
 			$messages = [
 				'type' => 'image',
 				'originalContentUrl' => 'https://graph.yuantathai.com/InvestorWebCharts/Modules/Standard.aspx?Provider=DDC&symbolnsources=ROJNA*BKK&Size=980*882&End=20170212&Start=&period=Daily&interval=1&Cycle=DAY1&Type=3&&username=KKS&password=KKS1234&pwdencode=false&Scale=0&EMA=10;50&MA=&OVER=;;OverlayV!;;&IND=MACD(12,26,9);RSI(14);;;&&COMP=&ShowBar=100&max=800&Skin=KKSLight&Layout=2Line;Default;Price;HisDate&Width=1&TimeStamp=17;17;1486808228.251',
@@ -50,3 +52,4 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK";
+echo "replyToken";
